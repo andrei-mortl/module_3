2ac3de614163fec6338503e1fe2c6f93f5bb859b
andrei-mortl <mor.tl08201988@gmail.com>
2024-10-31 16:46:19 +0300

    Add files via upload

--- module_3/module_3_2.py
+++ module_3/module_3_2.py
@@ -1,21 +1,25 @@
 def send_email(message, recipient, sender="university.help@gmail.com"):
 
     if sender == recipient:
-        return print('Нельзя отправить письмо самому себе!')
+        return 'Нельзя отправить письмо самому себе!'
 
     if sender == "university.help@gmail.com":
-        return print("Письмо успешно отправлено с адреса", sender,  "на адрес", recipient)
+        return "Письмо успешно отправлено с адреса", sender,  "на адрес", recipient
 
     symbols = ('@' and '.com' or '.ru' or '.net')
     a1 = sender
     a2 = recipient
     if not a1.endswith(symbols) and not a2.endswith(symbols):
-        return print('Невозможно отправить письмо с адреса', sender, 'на адрес', recipient)
+        return 'Невозможно отправить письмо с адреса', sender, 'на адрес', recipient
 
     else:
-        return print("НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient)
+        return "НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса", sender, "на адрес", recipient
 
-send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
-send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
-send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
-send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+result1 = send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
+result2 = send_email('Вы видите это сообщение как лучший студент курса!', 'urban.fan@mail.ru', sender='urban.info@gmail.com')
+result3 = send_email('Пожалуйста, исправьте задание', 'urban.student@mail.ru', sender='urban.teacher@mail.uk')
+result4 = send_email('Напоминаю самому себе о вебинаре', 'urban.teacher@mail.ru', sender='urban.teacher@mail.ru')
+print(result1)
+print(result2)
+print(result3)
+print(result4)
--- module_3/module_3_5.py
+++ module_3/module_3_5.py
@@ -4,8 +4,7 @@ def get_multiplied_digits(number):
     first = int(str_number[0])
     if len(str_number) > 1:
         return first * get_multiplied_digits(int(str_number[1:]))
-    elif len(str_number) <= 1:
-        return first
+    return first
 
 result = get_multiplied_digits(40203)
 print(result)
